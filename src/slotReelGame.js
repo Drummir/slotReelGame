bbfd6efed189e9e22f131caaec76283068467698
Drummir <ivan-syomin@mail.ru>
2020-08-26 22:14:03 +0300

    Add comment

--- src/slotReelGame.js
+++ src/slotReelGame.js
@@ -56,7 +56,7 @@ export default class slotReelGame {
         this.lines[i][j].position.set(100 * (i + 1), 30 + j * 70);
         this.scene.addChild(this.lines[i][j]);
       }
-
+      // Animation
       gsap.to(this.lines[i], {y: 600, duration: 1 + i * 0.2, repeat: -1, yoyo: true });
     }
   } 
